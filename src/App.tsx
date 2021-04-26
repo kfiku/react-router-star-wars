2bfd4fc8cb866d1cfb345ad19132d97a9c297a71
Grzegorz Klimek <grzegorz@e-gaming.cz>
2021-04-26 10:09:15 +0200

    use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { FilmsList } from "./Films";
 import { HookSelect } from "./HookSelect";
 
